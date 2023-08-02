193e947b7c108b3d19fc9b08ba1a2d21a06b35ba
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-08-02 13:51:36 +0100

    pulumi: add config and imports

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as cloudflare from "@pulumi/cloudflare";
+
+// Config
+const config = new pulumi.Config();
+const indexDocument = config.require("indexDocument");
+const zoneId = config.require("zoneId");
+const subdomain = config.require("subdomain");
